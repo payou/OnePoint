205fba4e44dfe2ee3b0af966989b5b4127e7a520
unknown <olivierandrejc@gmail.com>
2019-06-12 13:10:24 +0200

    start

---
+++ src/point/Main.java
@@ -0,0 +1,12 @@
+package point;
+
+import point.business.Person;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        Person p = new Person("Al");
+    }
+}
